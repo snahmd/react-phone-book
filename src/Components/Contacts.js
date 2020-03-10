10470211f95549160a75cfb73f061fa58dcfa877
snahmd <sanahmed7@gmail.com>
2020-03-10 22:58:14 +0100

    stateless contacts component

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import List from './List';
 import Form from './Form';
 
-class Contacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    addContact: PropTypes.func
-  };
-
-  render() {
-    return (
+const Contacts = props => 
+    
       <div>
-        <List contacts={this.props.contacts}/>
-        <Form addContact={this.props.addContact}/>
+        <List contacts={props.contacts}/>
+        <Form addContact={props.addContact}/>
       </div>
-    );
-  }
-}
+ 
+
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  addContact: PropTypes.func
+};
 
 export default Contacts;
